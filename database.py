aaad47011c379aca56aec35952f9bbfcaf7e521a
Your Name <your_email@example.com>
2026-02-27 16:20:27 +0530

    feat: add mongodb, enhance UI

---
+++ database.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+
+client = MongoClient(MONGO_URI)
+db = client["slot_machine_db"]
+
+stats_collection = db["game_stats"]
